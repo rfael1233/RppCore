9a2ccd4440fc850306877b13f7f79e214a4a87ef
Rafael Henrique Melo Batista <batista.h@academico.ifrn.edu.br>
2022-07-12 16:16:16 -0300

    Interfase do jogo

---
+++ New Unity Project (2)/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerAndCameraPrefab;
+
+    [SerializeField] 
+    private string locationToLoad;
+
+    [SerializeField] 
+    private string guiScene;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene(guiScene);
+        //SceneManager.LoadScene(locationToLoad, LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(locationToLoad, LoadSceneMode.Additive).completed += operation =>
+        {
+            Vector3 startPosition = GameObject.Find("PlayerStart").transform.position;
+            
+            Instantiate(playerAndCameraPrefab, startPosition, Quaternion.identity);
+            
+        }; 
+
+        Vector3 startPosition = GameObject.Find("PlayerStart").transform.position;
+        Instantiate(playerAndCameraPrefab, startPosition, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
